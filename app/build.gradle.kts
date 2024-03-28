6cd174312122c94b3b2301d59c30eed7548bb587
moemaair <rageali12@gmail.com>
2024-03-28 17:58:14 +0300

    adding apollo graphql

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
-    id ("com.android.application")
-    id ("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
-    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.apollographql.apollo3").version("3.7.3")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
-
+apollo {
+    service("service") {
+        packageName.set("com.ibuqa")
+    }
+}
 
 android {
     namespace = "com.ibuqa.screening"
@@ -25,54 +29,29 @@ android {
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+
+
+    dependencies {
+
+        implementation("androidx.core:core-ktx:1.7.0")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+        implementation("androidx.activity:activity-compose:1.3.1")
+        implementation("androidx.compose.ui:ui:1.2.0")
+        implementation("androidx.compose.ui:ui-tooling-preview:1.2.0")
+        implementation("androidx.compose.material:material:1.2.0")
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.0")
+        debugImplementation("androidx.compose.ui:ui-tooling:1.2.0")
+        debugImplementation("androidx.compose.ui:ui-test-manifest:1.2.0")
+
+        implementation("com.apollographql.apollo3:apollo-runtime:3.7.3")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+
+        implementation("com.google.dagger:hilt-android:2.42")
+        kapt("com.google.dagger:hilt-android-compiler:2.42")
+        kapt("androidx.hilt:hilt-compiler:1.0.0")
+        implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     }
 }
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    // Dagger Hilt
-    implementation ("com.google.dagger:hilt-android:2.44.2")
-    kapt ("com.google.dagger:hilt-compiler:2.44.2")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
