72d74e4492fcd8b97826cf94b848bf953d3b3944
moemaair <rageali12@gmail.com>
2024-03-29 18:15:25 +0300

    adding graphql client

--- app/src/main/java/com/ibuqa/screening/MainActivity.kt
+++ app/src/main/java/com/ibuqa/screening/MainActivity.kt
@@ -11,7 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ibuqa.screening.ui.theme.IbuqascreeningTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/ibuqa/screening/data/ApolloGraphqlGithubClient.kt
@@ -0,0 +1,3 @@
+package com.ibuqa.screening.data
+
+class ApolloGraphqlGithubClient
