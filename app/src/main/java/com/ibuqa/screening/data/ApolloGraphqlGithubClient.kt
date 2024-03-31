da13dffb4ef9cd7e21950c79dfe510e4c4e31ff2
moemaair <rageali12@gmail.com>
2024-03-31 15:11:58 +0300

    Initial implementation of ApolloGraphqlGithubClient

--- app/src/main/java/com/ibuqa/screening/data/ApolloGraphqlGithubClient.kt
+++ app/src/main/java/com/ibuqa/screening/data/ApolloGraphqlGithubClient.kt
@@ -1,3 +1,13 @@
 package com.ibuqa.screening.data
 
-class ApolloGraphqlGithubClient
+import com.ibuqa.screening.domain.IssueClient
+import com.ibuqa.screening.domain.UserIssue
+
+class ApolloGraphqlGithubClient(
+    private val apolloGraphqlGithubClient: ApolloGraphqlGithubClient
+): IssueClient{
+    override suspend fun getIssues(): List<UserIssue> {
+        TODO("Not yet implemented")
+    }
+
+}
